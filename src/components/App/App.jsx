823b0a7f3d25364abf126e85d82a4ce3b537997b
Nataliia-Homyk324 <homyk324@gmail.com>
2024-03-27 07:07:25 +0200

    profile-end

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,9 @@
 import userDate from "../Profile/userData.json";
-import Container from "./App.styled.js";
 import Profile from "../Profile/Profile.jsx";
 
 export default function App() {
   return (
-    <Container>
+    <>
       <Profile
         username={userDate.username}
         tag={userDate.tag}
@@ -12,7 +11,7 @@ export default function App() {
         avatar={userDate.avatar}
         stats={userDate.stats}
       />
-      </Container>
+    </>
   );
 }
 
--- src/components/App/App.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const Container = styled.div`
-  padding: 20px 30px;
-`;
-
-export default Container;
