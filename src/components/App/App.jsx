fda403bdc6d6ebfdd333dbd6f6e1fffbb90fb2af
Nataliia-Homyk324 <homyk324@gmail.com>
2024-03-27 07:51:22 +0200

    TransactionHistory

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,8 @@ import userDate from "../Profile/userData.json";
 import Profile from "../Profile/Profile.jsx";
 import FriendList from "../FriendList/FriendList.jsx";
 import friends from "../FriendList/friends.json";
+import TransactionHistory from "../TransactionHistory/TransactionHistory.jsx";
+import transactions from "../TransactionHistory/transactions.json";
 
 export default function App() {
   return (
@@ -13,7 +15,8 @@ export default function App() {
         avatar={userDate.avatar}
         stats={userDate.stats}
       />
-       <FriendList friends={friends} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 }
